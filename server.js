0e7e7a0e6bfb4445986acd0dd8fb968cab1ae261
Ratul Chhibber <ratulchhibber@gmail.com>
2022-10-19 14:15:14 +0530

    * Server.js Express | Mongoose | body-parser * Establishing connection with mongoose

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const Note = require('./model/Note');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+//extended true -> Allows nested objects
+//extended false -> Does not allow nested objects
+app.use(bodyParser.json());
+
+const password = encodeURIComponent("");
+
+mongoose.connect(`mongodb+srv://ratulchhibber:${password}@cluster0.wcdbqsz.mongodb.net/notesdb`)
+    .then(function () {
+        // Home Route (/)
+        app.get("/", function (request, response) {
+            response.send("HomePage");
+        });
+
+       
+
+    });
+
+
+
+app.listen(5055, function () {
+    console.log("Server started, Port: 5055");
+});
