44555cca732115db40cfe4985b84e32a18d53ea7
Ratul Chhibber <ratulchhibber@gmail.com>
2022-10-20 18:42:53 +0530

    * Error handling- Error would now gives custom json

--- routes/Note.js
+++ routes/Note.js
@@ -1,33 +1,58 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Note = require('./../model/Note');
+const Note = require("./../model/Note");
 
-router.get("/list/all", async function (request, response) {
+router.get("/", function (request, response, next) {
+  try {
+    if (request.params.something == "") {
+      response.send("@Homepage");
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/list/all", async function (request, response, next) {
+  try {
     var notes = await Note.find();
     response.json(notes);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get("/list/:userid", async function (request, response) {
+router.get("/list/:userid", async function (request, response, next) {
+  try {
     var notes = await Note.find({ userid: request.params.userid });
     response.json(notes);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post("/add", async function (request, response) {
-    await Note.deleteOne({ id: request.body.id });
-
+router.post("/add", async function (request, response, next) {
+  await Note.deleteOne({ id: request.body.id });
+  try {
     const newNote = new Note({
-        id: request.body.id,
-        userid: request.body.userid,
-        title: request.body.title,
-        content: request.body.content
+      id: request.body.id,
+      userid: request.body.userid,
+      title: request.body.title,
+      content: request.body.content,
     });
     await newNote.save();
     response.json({ message: "New note created " + `${request.body.id}` });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.delete("/delete/:id", async function (request, response) {
+router.delete("/delete/:id", async function (request, response, next) {
+  try {
     await Note.deleteOne({ id: request.params.id });
     response.json({ message: "Note deleted " + `${request.params.id}` });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const Note = require('./model/Note');
-const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const Note = require("./model/Note");
+const bodyParser = require("body-parser");
+const { handleError } = require("./error/error");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //extended true -> Allows nested objects
 //extended false -> Does not allow nested objects
 app.use(bodyParser.json());
 
-const password = encodeURIComponent(`${process.env.MONGODB_KEY}`);
+app.use((req, res, next) => {
+  const { method, path } = req;
+  console.log(`New request to ${method} ${path}`);
+  next();
+});
 
-mongoose.connect(`mongodb+srv://ratulchhibber:${password}@cluster0.wcdbqsz.mongodb.net/notesdb`)
-    .then(function () {
-        // Home Route (/)
-        app.get("/", function (request, response) {
-            response.send("HomePage");
-        });
+const password = encodeURIComponent("Ratul@Mongodb01");
 
-        const noteRouter = require('./routes/Note');
-        app.use("/notes", noteRouter);
+mongoose
+  .connect(
+    `mongodb+srv://ratulchhibber:${password}@cluster0.wcdbqsz.mongodb.net/notesdb`
+  )
+  .then(function () {
+    app.get("/", function (request, response) {
+      response.send("Homepage");
     });
 
+    const noteRouter = require("./routes/Note");
+    app.use("/notes", noteRouter);
+    app.use(handleError);
+  });
 
 const PORT = process.env.PORT || 5055;
 app.listen(PORT, function () {
-    console.log(`Server started, Port: ${PORT}`);
+  console.log(`Server started, Port: ${PORT}`);
 });
